b04a0da3f1809ade7ee19f1eeceb678d61b59b53
Nguyễn Thái Hoà <hoa41300@gmail.com>
2025-06-02 14:54:30 +0700

    Add JwtOptions

---
+++ src/RustRetail.IdentityService.Infrastructure/Authentication/Jwt/JwtOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace RustRetail.IdentityService.Infrastructure.Authentication.Jwt
+{
+    internal class JwtOptions
+    {
+        public string Issuer { get; init; } = string.Empty!;
+        public string Audience { get; init; } = string.Empty!;
+        public string SecretKey { get; init; } = string.Empty!;
+        public int AccessTokenExpiryInMilliseconds { get; init; }
+        public int RefreshTokenExpiryInMilliseconds { get; init; }
+    }
+}
