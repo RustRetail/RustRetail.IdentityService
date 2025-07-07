4830ea72b191810f414e7c4804aefb77eb65f7b8
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-07-07 11:41:18 +0700

    Update Serilog's Logging with Scope

--- src/RustRetail.IdentityService.Infrastructure/BackgroundJobs/Quartz/OutboxPublisherJob.cs
+++ src/RustRetail.IdentityService.Infrastructure/BackgroundJobs/Quartz/OutboxPublisherJob.cs
@@ -22,46 +22,52 @@ namespace RustRetail.IdentityService.Infrastructure.BackgroundJobs.Quartz
 
         public async Task Execute(IJobExecutionContext context)
         {
-            _logger.LogInformation("OutboxPublisherJob started at {Time}.", DateTimeOffset.UtcNow);
-            using var scope = _scopeFactory.CreateScope();
-            var outboxMessageService = scope.ServiceProvider.GetRequiredService<IOutboxMessageService>();
-            var messageBus = scope.ServiceProvider.GetRequiredService<IMessageBus>();
-            var messages = await outboxMessageService.GetUnpublishedMessagesAsync(
-                takeSize: 20,
-                asTracking: true,
-                cancellationToken: context.CancellationToken);
-            foreach (var message in messages)
+            using (_logger.BeginScope(new Dictionary<string, object>
             {
-                var type = Type.GetType(message.Type);
-                if (type == null)
+                ["Scope"] = nameof(OutboxPublisherJob)
+            }))
+            {
+                _logger.LogInformation("OutboxPublisherJob started at {Time}.", DateTimeOffset.UtcNow);
+                using var scope = _scopeFactory.CreateScope();
+                var outboxMessageService = scope.ServiceProvider.GetRequiredService<IOutboxMessageService>();
+                var messageBus = scope.ServiceProvider.GetRequiredService<IMessageBus>();
+                var messages = await outboxMessageService.GetUnpublishedMessagesAsync(
+                    takeSize: 20,
+                    asTracking: true,
+                    cancellationToken: context.CancellationToken);
+                foreach (var message in messages)
                 {
-                    _logger.LogError("Failed to resolve type '{Type}' for message ID {MessageId}.", message.Type, message.Id);
-                    message.Error = "Invalid type.";
-                    outboxMessageService.UpdateOutboxMessage(message);
-                    continue;
-                }
-                var @event = JsonConvert.DeserializeObject(message.Content, type);
-                if (@event == null)
-                {
-                    _logger.LogError("Failed to deserialize message with ID {MessageId} and Type {MessageType}.", message.Id, message.Type);
-                    message.Error = "Deserialization failed.";
-                    outboxMessageService.UpdateOutboxMessage(message);
-                    continue;
-                }
-                try
-                {
-                    await messageBus.PublishAsync(@event, type, context.CancellationToken);
-                    message.ProcessedOn = DateTimeOffset.UtcNow;
+                    var type = Type.GetType(message.Type);
+                    if (type == null)
+                    {
+                        _logger.LogError("Failed to resolve type '{Type}' for message ID {MessageId}.", message.Type, message.Id);
+                        message.Error = "Invalid type.";
+                        outboxMessageService.UpdateOutboxMessage(message);
+                        continue;
+                    }
+                    var @event = JsonConvert.DeserializeObject(message.Content, type);
+                    if (@event == null)
+                    {
+                        _logger.LogError("Failed to deserialize message with ID {MessageId} and Type {MessageType}.", message.Id, message.Type);
+                        message.Error = "Deserialization failed.";
+                        outboxMessageService.UpdateOutboxMessage(message);
+                        continue;
+                    }
+                    try
+                    {
+                        await messageBus.PublishAsync(@event, type, context.CancellationToken);
+                        message.ProcessedOn = DateTimeOffset.UtcNow;
 
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Failed to publish message ID {MessageId}.", message.Id);
+                        message.Error = ex.Message;
+                    }
                 }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Failed to publish message ID {MessageId}.", message.Id);
-                    message.Error = ex.Message;
-                }
+                await outboxMessageService.SaveChangesAsync(context.CancellationToken);
+                _logger.LogInformation("OutboxPublisherJob completed at {Time}. Processed {Count} messages.", DateTimeOffset.UtcNow, messages.Count);
             }
-            await outboxMessageService.SaveChangesAsync(context.CancellationToken);
-            _logger.LogInformation("OutboxPublisherJob completed at {Time}. Processed {Count} messages.", DateTimeOffset.UtcNow, messages.Count);
         }
     }
 }
